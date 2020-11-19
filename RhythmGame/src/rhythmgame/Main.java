b2baf8af0fce8dda91cd8cea74bfd056b46106b0
oh-aram <oalam2@naver.com>
2020-11-19 21:48:49 +0900

    ww

--- RhythmGame/src/rhythmgame/Main.java
+++ RhythmGame/src/rhythmgame/Main.java
@@ -6,7 +6,7 @@ public class Main {
    
    public static final int SCREEN_WIDTH = 1280;
    public static final int SCREEN_HEIGHT = 720;
-   public static final int NOTE_SPEED = 3;
+   public static final int NOTE_SPEED = 2;
    public static final int SLEEP_TIME = 8;
    public static final int REACH_TIME = 2; //노트가 판정바에 도달하기까지의 시간
    
