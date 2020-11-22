add69b9756128bff390d2c6069231bad00299632
kyuyoung <kyuyoung@114.204.162.48>
2020-11-23 03:15:52 +0900

    change file

--- RhythmGame/src/rhythmgame/ChatMsg.java
+++ RhythmGame/src/rhythmgame/ChatMsg.java
@@ -1,5 +1,5 @@
 package rhythmgame;
-<<<<<<< HEAD
+
 
 // ChatMsg.java 채팅 메시지 ObjectStream 용.
 import java.io.Serializable;
@@ -45,18 +45,4 @@ class ChatMsg implements Serializable {
 	public void setImg(ImageIcon img) {
 		this.img = img;
 	}
-=======
-// ChatMsg.java 채팅 메시지 ObjectStream 용.
-public class ChatMsg {
-    public String code; // 100:로그인, 400:로그아웃, 200:채팅메시지, 300:Image, 500: Mouse Event
-    public String UserName;
-    public String data;
-    public byte[] imgbytes;
-
-    public ChatMsg(String UserName, String code, String msg) {
-        this.code = code;
-        this.UserName = UserName;
-        this.data = msg;
-    }
->>>>>>> refs/remotes/origin/kyuyoung
 }
